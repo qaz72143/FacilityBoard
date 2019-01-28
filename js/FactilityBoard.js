554a88c2a2bc00ea78c4923aced66953d510dea6
qaz72143 <46678641+qaz72143@users.noreply.github.com>
2019-01-28 17:18:24 +0800

    Update FactilityBoard.js

--- js/FactilityBoard.js
+++ js/FactilityBoard.js
@@ -269,7 +269,7 @@ function Init_data()	//初始化陣列內容型態
 	        dataType: 'text',
 	        success: function(data_t) {
 	            //alert(data_t);
-	        	console.log(data);
+	        	console.log(data_t);
 	        	data = data_t.split("\n");
 	        }
 	    });

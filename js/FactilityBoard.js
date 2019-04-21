ac7abae0d62582234c1201268ecbbe3513dcd5ff
qaz72143 <qaz72143@yahoo.com.tw>
2019-04-21 12:40:49 +0800

    MachineData.txt

--- js/FactilityBoard.js
+++ js/FactilityBoard.js
@@ -191,12 +191,12 @@ function showData()
 	}
 	
 	//刪掉多餘的機台格子
-	var deleteMachine;
+	/*var deleteMachine;
 	for(var i=MachineQty+1; i<100; i++)
 	{
 		deleteMachine = document.getElementById('MachineData-' + i);
 		deleteMachine.parentNode.removeChild(deleteMachine);		
-	}
+	}*/
 	
 	//處理Chart		li 
 	var ConnectCount = document.getElementById('ConnectCnt');
